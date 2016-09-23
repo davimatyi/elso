0b9db47c7b599b9d3440c1719b21bcee75594644
bs <spectical@freemail.hu>
2016-09-23 15:46:51 +0200

    Tanár úr Actor osztálya

---
+++ core/src/hu/tokin_game/elso/BSActor.java
@@ -0,0 +1,17 @@
+package hu.tokin_game.elso;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * Created by bs on 2016.09.23..
+ */
+
+public class BSActor extends Actor {
+    public BSActor() {
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+    }
+}
