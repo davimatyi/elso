1327c5d89c30e4ecc70ed76a32ec78b445b27819
Dávid Mátyás <Dvid Mtys>
2016-09-23 14:31:55 +0200

    Initial commit

---
+++ android/src/hu/tokin_game/elso/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package hu.tokin_game.elso;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import hu.tokin_game.elso.Fo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Fo(), config);
+	}
+}
---
+++ desktop/src/hu/tokin_game/elso/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package hu.tokin_game.elso.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import hu.tokin_game.elso.Fo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Fo(), config);
+	}
+}
